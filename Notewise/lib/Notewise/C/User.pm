16847f7051a04af88ae1a9acb9c22e18a1d0a5f8
notewise <notewise@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-01-16 08:04:28 +0000

    added user specific home page

--- Notewise/lib/Notewise/C/User.pm
+++ Notewise/lib/Notewise/C/User.pm
@@ -26,6 +26,36 @@ sub login : Local {
     $c->stash->{template} = 'User/login.tt';
 }
 
+sub home : Local {
+    my ( $self, $c, $username ) = @_;
+    my $user = Notewise::M::CDBI::User->search(username=>lc($username))->first;
+    unless($user->id == $c->req->{user_id}){
+        # TODO - need to make a different version for the public
+        die "Sorry, you don't have permission to look at that user's homepage";
+    }
+    $c->stash->{user}=$user;
+
+    my @lastviewed=map $_->object, Notewise::M::CDBI::ObjectId->search(user=>$user->id,type=>'kernel');
+    # XXX this sort is going to be dog slow - need to swap this out for some actual sql
+    @lastviewed=sort {$b->lastviewed <=> $a->lastviewed} @lastviewed;
+    my $max = 5;
+    if(@lastviewed < $max){
+        $max = @lastviewed;
+    }
+    $c->stash->{lastviewed}=[@lastviewed[0..($max-1)]];
+
+    my @lastcreated=map $_->object, Notewise::M::CDBI::ObjectId->search(user=>$user->id,type=>'kernel');
+    # XXX this sort is going to be dog slow - need to swap this out for some actual sql
+    @lastcreated=sort {$b->created <=> $a->created} @lastcreated;
+    $max = 5;
+    if(@lastcreated < $max){
+        $max = @lastcreated;
+    }
+    $c->stash->{lastcreated}=[@lastcreated[0..($max-1)]];
+
+    $c->stash->{template} = 'User/home.tt';
+}
+
 =back
 
 =head1 AUTHOR
