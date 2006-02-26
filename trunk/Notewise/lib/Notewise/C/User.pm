17dba19341674f436738d63507ec3703071cc6d5
scotty <scotty@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-02-26 08:36:47 +0000

    optimized queries for lastViewed and lastCreated. Closes #207

--- trunk/Notewise/lib/Notewise/C/User.pm
+++ trunk/Notewise/lib/Notewise/C/User.pm
@@ -36,18 +36,8 @@ sub home : Local {
         die "Sorry, you don't have permission to look at that user's homepage";
     }
     $c->stash->{user}=$user;
-
     $c->stash->{lastviewed}=[$c->model('CDBI::Kernel')->most_recently_viewed_kernel($user->id,15)];
-
-    my @lastcreated=map $_->object, $c->model('CDBI::ObjectId')->search(user=>$user->id,type=>'kernel');
-    # XXX this sort is going to be dog slow - need to swap this out for some actual sql
-    @lastcreated=sort {$b->created <=> $a->created} @lastcreated;
-    my $max = 15;
-    if(@lastcreated < $max){
-        $max = @lastcreated;
-    }
-    $c->stash->{lastcreated}=[@lastcreated[0..($max-1)]];
-
+    $c->stash->{lastcreated}=[$c->model('CDBI::Kernel')->most_recently_created_kernel($user->id,15)];
     $c->stash->{template} = 'User/home.tt';
 }
 
