6f62358acd5a86bcbd502e344b9f9152d11d96b0
scotty <scotty@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-02-26 07:53:10 +0000

    fixed borked rel twistie. Fixes #232

--- Notewise/root/js/ui.js
+++ Notewise/root/js/ui.js
@@ -1,31 +1,34 @@
 function expandMenu(button, paneId) {
-	var pane = document.getElementById(paneId);
-	var sandbox_container = document.getElementById('sandbox_container');
-	var sandbox = document.getElementById('sandbox');
-	
-	var opts = {
-	    duration: 50,
-	    onComplete: function () {
-	        sandbox_container.style.height = "100%";
-	        sandbox.style.height = "100%"; 
-	    }
-	}
-	
-	if(pane.__expand == null) {
-		pane.__expand = new fx.Height(pane, opts);
-	}
-
-	pane.__expand.toggle();
-	if(pane.hidden) {
-	    Element.removeClassName(button, 'open');
-	    Element.addClassName(button, 'closed');
-	    pane.style.borderBottom = "0";
-  } else {
-      // TODO switch this for the regex version in visiblekernel.js
-	    Element.removeClassName(button, 'closed');
-	    Element.addClassName(button, 'open');
-      pane.style.borderBottom = "1px solid #bebebe";
-  }
+    var pane = document.getElementById(paneId);
+    var sandbox_container = document.getElementById('sandbox_container');
+    var sandbox = document.getElementById('sandbox');
     
-	sandbox.style.height = "100%";
+    var opts = {
+        duration: 50,
+        onComplete: function () {
+            sandbox_container.style.height = "100%";
+            sandbox.style.height = "100%"; 
+        }
+    }
+    
+//    if(pane.__expand == null) {
+//        pane.__expand = new fx.Height(pane, opts);
+//    }
+
+//    pane.__expand.toggle();
+    if(Element.hasClassName(pane,'open')) {
+        Element.removeClassName(pane, 'open');
+        Element.addClassName(pane, 'closed');
+        Element.removeClassName(button, 'open');
+        Element.addClassName(button, 'closed');
+        pane.style.borderBottom = "0";
+    } else {
+        Element.removeClassName(pane, 'closed');
+        Element.addClassName(pane, 'open');
+        Element.removeClassName(button, 'closed');
+        Element.addClassName(button, 'open');
+        pane.style.borderBottom = "1px solid #bebebe";
+    }
+    
+    sandbox.style.height = "100%";
 }
