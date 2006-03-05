2657467b3d043e2b02f23e596f4ad3aca38158e5
scotty <scotty@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-03-05 02:03:54 +0000

    removed username and authenhash from plugin

--- trunk/Notewise/lib/Notewise/C/Plugins.pm
+++ trunk/Notewise/lib/Notewise/C/Plugins.pm
@@ -5,8 +5,6 @@ use base 'Catalyst::Base';
 
 sub firefox : Path('/plugins/notewise.src') {
     my ( $self, $c ) = @_;
-    $c->stash->{username} = $c->user->user->username;
-    $c->stash->{authenhash} = $c->user->user->authenhash;
     $c->stash->{template} = 'plugins/firefox-search.tt';
 }
 
