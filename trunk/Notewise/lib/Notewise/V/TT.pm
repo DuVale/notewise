b1e5ae941e9e75df3cc9ca71a3b90885ec7a7344
scotty <scotty@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-03-21 22:06:11 +0000

    more performance tweaks - turned on POST_CHOMP and PRE_CHOMP. Started caching object() on M::CDBI::ObjectId. Refs #132

--- trunk/Notewise/lib/Notewise/V/TT.pm
+++ trunk/Notewise/lib/Notewise/V/TT.pm
@@ -5,6 +5,9 @@ use base 'Catalyst::View::TT';
 __PACKAGE__->config->{EVAL_PERL} = 1;
 __PACKAGE__->config->{TIMER} = 0;
 __PACKAGE__->config->{RECURSION} = 1;
+__PACKAGE__->config->{PRE_CHOMP} = 2; # collapse whitespace before tags to 1 space
+__PACKAGE__->config->{POST_CHOMP} = 2; # collapse whitespace after tags to 1 space
+__PACKAGE__->config->{COMPILE_EXT} = '.ttc';
 
 =head1 NAME
 
