100476ad4dd2245e0dc7a81ae20582958e7a6ac5
scotty <scotty@240cfaf6-7638-0410-a40e-cf6897baf8d7>
2006-03-28 03:12:45 +0000

    added quick help popup and fixed problem with notes not looking selected. Closes #406

--- Notewise/root/js/bugs.js
+++ Notewise/root/js/bugs.js
@@ -1,12 +1,16 @@
 var Bugs = {};
-Bugs.show_window = function(){
+Bugs.show_window = function(location){
     var iframe = document.createElement('iframe');
-    iframe.id = 'bugreport';
-    iframe.src = '/user/bug_report';
+    iframe.id = location;
+    iframe.className = 'popover';
+    iframe.src = location;
     var body=document.getElementsByTagName('body')[0];
     body.appendChild(iframe);
 };
+
 Bugs.close_window = function() {
-    var iframe = parent.document.getElementById('bugreport');
-    iframe.parentNode.removeChild(iframe);
+    var iframe = parent.document.getElementsByClassName('popover')[0];
+    if(iframe){
+        iframe.parentNode.removeChild(iframe);
+    }
 };
